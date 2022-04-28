8031b7e00c3f8197717e39d0a77fcbd55cd0ac79
timothyc3 <timothyc2@Gmail.com>
2022-04-28 15:43:36 +0800

    Added button in the form to allow user to return to main menu.

--- src/components/LeaderboardForm.js
+++ src/components/LeaderboardForm.js
@@ -1,11 +1,14 @@
 import './LeaderboardForm.css';
 import React from "react";
 import Timer from "./Timer";
+import { Link } from "react-router-dom";
 
 export default function LeaderboardForm(props) {
+
     return (
         <div className='form-container' style={props.styles}>
             <form action="" style={props.displayMode}>
+                <span className='accent'></span>
                 <div className="score">
                     <h1>Your time:</h1>
                     <Timer time={props.time}/>
@@ -18,6 +21,7 @@ export default function LeaderboardForm(props) {
                     
                     <li>
                         <input type="submit"/>
+                        <h3>or <Link to='/'>return to main menu</Link></h3>
                     </li>
                 </ul>
             </form>
