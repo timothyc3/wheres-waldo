cf5e885206b60b83d3b436883a70fb6033068a2f
timothyc3 <timothyc2@Gmail.com>
2022-04-28 18:01:24 +0800

    submit button now disables itself once the user has submitted their score. The invalid message now does not appear until the user has inputted something.

--- src/components/LeaderboardForm.js
+++ src/components/LeaderboardForm.js
@@ -1,5 +1,5 @@
 import './LeaderboardForm.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Timer from "./Timer";
 import { Link } from "react-router-dom";
 import { handleScoreSubmit } from "../firebase";
@@ -8,6 +8,19 @@ export default function LeaderboardForm(props) {
 
     const [name, setName] = useState('');
     const [submitted, setSubmitted] = useState(false);
+    const [nameError, setNameError] = useState('invalid')
+
+    const [minNameLength, maxNameLength] = [3, 20]
+
+    useEffect(() => {
+        if (name.length === 0) {
+            setNameError('Please fill in name')
+        }
+
+        else if (name.length < minNameLength) {
+            setNameError('Name needs to above 3 characters');
+        }
+    }, [name]);
 
     return (
         <div className='form-container' style={props.styles}>
@@ -20,21 +33,30 @@ export default function LeaderboardForm(props) {
                 <ul>
                     <li>
                         <label htmlFor="name">Add your name:</label>
-                        <input type="text" name='name' id='name' onInput={(event) => {
-                            setName(event.target.value)
-                        }}/>
+                        <input type="text" name='name' id='name' autoComplete='off' required
+                               minLength={minNameLength} maxLength={maxNameLength}
+                               placeholder=''
+                               onInput={(event) => {
+                                   setName(event.target.value);
+                                    console.log(submitted)}
+                        }/>
+                        <h4>* {nameError}</h4>
                     </li>
                     
                     <li>
-                        <input type="submit" onClick={(event) => {
-                            event.preventDefault();
-                            if (submitted === false) {
-                                handleScoreSubmit({name: name, time: props.time}).then( () => {
-                                    setSubmitted(true);
-                                });
-                            }
-                        }}/>
-                        <h3>or <Link to='/'>return to main menu</Link></h3>
+                        <input type="submit"
+                               disabled={submitted}
+                               onClick={(event) => {
+                                    event.preventDefault();
+                                    if (submitted === false && name.length >= minNameLength && name.length <= maxNameLength) {
+                                        handleScoreSubmit({name: name, time: props.time}).then( () => {
+                                            setSubmitted(true);
+                                        });
+                                    }
+                                    else {
+                                        console.log('invalid submission', submitted)}
+                               }}/>
+                        <h3>{submitted ? '' : 'or '}<Link to='/'>return to main menu</Link></h3>
                         <div className='submit-message'
                              style={{visibility: submitted ? 'visible' : 'hidden'}}>
                             <ion-icon name="checkmark-circle-outline"></ion-icon>
