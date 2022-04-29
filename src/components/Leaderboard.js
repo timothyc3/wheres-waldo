c559933de2e90c619fd996cac77bd53f042dbd31
timothyc3 <timothyc2@Gmail.com>
2022-04-29 16:51:26 +0800

    Added table styling and the ability to call a specific level's leaderboard.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,30 +1,51 @@
 import './Leaderboard.css';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { getLeaderboardData } from "../firebase";
 
 export default function Leaderboard() {
 
-    const [data, setData] = useState(null);
+    // data is used to store data that is called from the backend from useEffect.
+    const [data, setData] = useState([]);
+
+    // stores the current level being filtered by the user, defaults to the first level
+    const [levelFilter, setLevelFilter] = useState(1);
 
     useEffect(() => {
         getLeaderboardData()
             .then((result) => {setData(result)});
     }, []);
 
-    useEffect(() => {
-        console.log(data)
-    }, [data])
+    // generate the rows to display on the jsx.
+    const tableRows = data.filter(entry => entry.level === levelFilter).map(
+        filteredEntry => <tr>
+            <td>{filteredEntry.name}</td>
+            <td>{filteredEntry.time}</td>
+        </tr>
+    )
+
+    function handleFilterClick(event) {
+        setLevelFilter(parseInt(event.target.textContent));
+    }
 
     return (
         <div className='leaderboard-page'>
-            <div className='levels-filter'>
-
-            </div>
+            <ul className='levels-filter'>
+                <li onClick={(event) => {handleFilterClick(event)}}>1</li>
+                <li onClick={(event) => {handleFilterClick(event)}}>2</li>
+                <li onClick={(event) => {handleFilterClick(event)}}>3</li>
+                <li onClick={(event) => {handleFilterClick(event)}}>4</li>
+            </ul>
             <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Time</th>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableRows}
+                </tbody>
+
             </table>
         </div>
 
